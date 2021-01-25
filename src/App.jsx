0c365f57f767dd8263969e87c4b50fd6c5870975
clickf5 <paninhome@gmail.com>
2021-01-25 14:45:54 +0300

    add layout

--- src/App.jsx
+++ src/App.jsx
@@ -38,6 +38,27 @@ const App = (props) => {
           {renderedChannels}
         </ul>
       </div>
+      <div className="col h-100">
+        <div className="d-flex flex-column h-100">
+          <div id="messages=box" className="chat-messages overflow-auto mb-3">
+            <div className="text-break">
+              <b>Jolie_DAmore71</b>
+              : fsdfgsdfg
+            </div>
+          </div>
+          <div className="mt-auto">
+            <form noValidate="" className="">
+              <div className="form-group">
+                <div className="input-group">
+                  <input name="body" aria-label="body" className="mr-2 form-control" value="" />
+                  <button aria-label="submit" type="submit" className="btn btn-primary">Submit</button>
+                  <div className="d-block invalid-feedback">&nbsp;</div>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
