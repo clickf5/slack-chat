513e7e8e6154e03c13f19319969585143e966a83
clickf5 <paninhome@gmail.com>
2021-01-27 16:52:38 +0300

    add input focus

--- src/components/messages/SendMessageForm.jsx
+++ src/components/messages/SendMessageForm.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useFormik } from 'formik';
 import axios from 'axios';
@@ -7,6 +7,12 @@ import AuthContext from '../../contexts/AuthContext';
 import routes from '../../routes';
 
 const SendMessageForm = () => {
+  const inputBody = useRef(null);
+
+  useEffect(() => {
+    inputBody.current.focus();
+  });
+
   const { nickname } = useContext(AuthContext);
 
   const dispatch = useDispatch();
@@ -26,6 +32,7 @@ const SendMessageForm = () => {
       await axios.post(channelMessagesPath, { data });
       dispatch(addMessage(message));
       resetForm({});
+      inputBody.current.focus();
     } catch (e) {
       console.log(e);
     }
@@ -46,6 +53,7 @@ const SendMessageForm = () => {
             className="mr-2 form-control"
             value={formik.values.body}
             onChange={formik.handleChange}
+            ref={inputBody}
           />
           <button aria-label="submit" type="submit" className="btn btn-primary" disabled={formik.isSubmitting}>Submit</button>
           <div className="d-block invalid-feedback">&nbsp;</div>
