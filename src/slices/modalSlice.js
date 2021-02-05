4fc879a4c8eb46c9055905146810af6ae72439b7
clickf5 <paninhome@gmail.com>
2021-02-05 09:01:51 +0300

    add modal slice

---
+++ src/slices/modalSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const modalSlice = createSlice({
+  name: 'modal',
+  initialState: { isOpened: false, type: null, extra: null },
+  reducers: {
+    openModal: (state, action) => {
+      const { type, extra } = action.payload;
+      return {
+        ...state, isOpened: true, type, extra,
+      };
+    },
+    closeModal: (state) => ({
+      ...state, isOpened: false, type: null, extra: null,
+    }),
+  },
+});
+
+export const { setModal } = modalSlice.actions;
+
+export default modalSlice.reducer;
